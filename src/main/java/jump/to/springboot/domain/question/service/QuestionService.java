ced81611af61aee86f77e1f963afa065e3498fe8
min429 <lsm990429@gmail.com>
2025-01-24 15:25:51 +0900

    상세 페이지 만들기

--- src/main/java/jump/to/springboot/domain/question/service/QuestionService.java
+++ src/main/java/jump/to/springboot/domain/question/service/QuestionService.java
@@ -1,20 +1,31 @@
 package jump.to.springboot.domain.question.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import jump.to.springboot.domain.question.entity.Question;
 import jump.to.springboot.domain.question.repository.QuestionRepository;
+import jump.to.springboot.global.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
 
-    private final QuestionRepository questionRepository;
+	private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
-    }
+	public List<Question> getList() {
+		return this.questionRepository.findAll();
+	}
+
+	public Question getQuestion(Integer id) {
+		Optional<Question> question = this.questionRepository.findById(id);
+		if (question.isPresent()) {
+			return question.get();
+		} else {
+			throw new DataNotFoundException("question not found");
+		}
+	}
 }
