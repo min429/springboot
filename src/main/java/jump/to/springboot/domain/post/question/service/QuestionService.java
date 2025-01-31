358dacbf07d654ee093668f633a78eefab3f693d
min429 <lsm990429@gmail.com>
2025-01-31 15:46:20 +0900

    검색 기능 추가하기

--- src/main/java/jump/to/springboot/domain/post/question/service/QuestionService.java
+++ src/main/java/jump/to/springboot/domain/post/question/service/QuestionService.java
@@ -9,8 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import jump.to.springboot.domain.post.answer.entity.Answer;
 import jump.to.springboot.domain.post.question.entity.Question;
 import jump.to.springboot.domain.post.question.repository.QuestionRepository;
 import jump.to.springboot.domain.user.entity.SiteUser;
@@ -22,10 +30,6 @@ import lombok.RequiredArgsConstructor;
 public class QuestionService {
 
 	private final QuestionRepository questionRepository;
-
-	public List<Question> getList() {
-		return this.questionRepository.findAll();
-	}
 
 	public Question getQuestion(Integer id) {
 		Optional<Question> question = this.questionRepository.findById(id);
@@ -45,11 +49,12 @@ public class QuestionService {
 		this.questionRepository.save(q);
 	}
 
-	public Page<Question> getList(int page) {
-		List<Sort.Order> sorts = new ArrayList<>(); // 정렬 조건 리스트
+	public Page<Question> getList(int page, String kw) {
+		List<Sort.Order> sorts = new ArrayList<>();
 		sorts.add(Sort.Order.desc("createDate"));
 		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-		return this.questionRepository.findAll(pageable);
+		Specification<Question> spec = search(kw);
+		return this.questionRepository.findAllByKeyword(kw, pageable);
 	}
 
 	public void modify(Question question, String subject, String content) {
@@ -66,5 +71,24 @@ public class QuestionService {
 	public void vote(Question question, SiteUser siteUser) {
 		question.getVoter().add(siteUser);
 		this.questionRepository.save(question);
+	}
+
+	private Specification<Question> search(String kw) {
+		return new Specification<>() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				query.distinct(true);  // 중복을 제거
+				Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+				Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+				Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+				return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), // 제목
+					cb.like(q.get("content"), "%" + kw + "%"),      // 내용
+					cb.like(u1.get("username"), "%" + kw + "%"),    // 질문 작성자
+					cb.like(a.get("content"), "%" + kw + "%"),      // 답변 내용
+					cb.like(u2.get("username"), "%" + kw + "%"));   // 답변 작성자
+			}
+		};
 	}
 }
